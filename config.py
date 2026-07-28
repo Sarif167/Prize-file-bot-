71df00da33301d6d13305d88d9ac35b6e8fcab7e
Botmaster55 <wajsarif461@gmail.com>
2026-07-28 07:28:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,21 +1,28 @@
-# config.py
+import os
 
-API_ID = 12345678  # My.telegram.org se lein
-API_HASH = "YOUR_API_HASH_HERE"
-BOT_TOKEN = "YOUR_BOT_TOKEN_HERE"
+# Config Variables
+API_ID = int(os.environ.get("API_ID", "12345678"))
+API_HASH = os.environ.get("API_HASH", "YOUR_API_HASH")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "YOUR_BOT_TOKEN")
 
-# Shortener Config
-SHORTENER_URL = "https://yourshortener.com/api"
-SHORTENER_API = "YOUR_SHORTENER_API_KEY"
+SHORTENER_URL = os.environ.get(
+    "SHORTENER_URL", "https://yourshortener.com/api"
+)
+SHORTENER_API = os.environ.get("SHORTENER_API", "YOUR_SHORTENER_API_KEY")
 
-# Bot Links & Usernames
-BOT_USERNAME = "YourMovieBot"  # Bina @ ke
-MAIN_CHANNEL = "https://t.me/YourMainChannel"  # Main Channel Link
-DEV_ADMIN_USERNAME = "YourAdminUsername"  # Admin/Developer Username
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "YourMovieBot")
+MAIN_CHANNEL = os.environ.get(
+    "MAIN_CHANNEL", "https://t.me/YourMainChannel"
+)
+DEV_ADMIN_USERNAME = os.environ.get("DEV_ADMIN_USERNAME", "YourAdminUsername")
 
-# Welcome Image URL
-START_PHOTO = "https://i.ibb.co/L8xX11d/movie-banner.jpg"
+START_PHOTO = os.environ.get(
+    "START_PHOTO", "https://i.ibb.co/L8xX11d/movie-banner.jpg"
+)
 
-# Database Storage File (Simple SQLite Database)
+# How To Verify Video/Tutorial Link (YouTube ya Telegram Post Link)
+HOW_TO_VERIFY_URL = os.environ.get(
+    "HOW_TO_VERIFY_URL", "https://t.me/YourMainChannel/123"
+)
+
 DB_NAME = "movie_bot.db"
-
