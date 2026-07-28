fb5f32df738b0752b73434249c3615fbf4518e3d
Botmaster55 <wajsarif461@gmail.com>
2026-07-28 07:20:00 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,21 @@
+# config.py
+
+API_ID = 12345678  # My.telegram.org se lein
+API_HASH = "YOUR_API_HASH_HERE"
+BOT_TOKEN = "YOUR_BOT_TOKEN_HERE"
+
+# Shortener Config
+SHORTENER_URL = "https://yourshortener.com/api"
+SHORTENER_API = "YOUR_SHORTENER_API_KEY"
+
+# Bot Links & Usernames
+BOT_USERNAME = "YourMovieBot"  # Bina @ ke
+MAIN_CHANNEL = "https://t.me/YourMainChannel"  # Main Channel Link
+DEV_ADMIN_USERNAME = "YourAdminUsername"  # Admin/Developer Username
+
+# Welcome Image URL
+START_PHOTO = "https://i.ibb.co/L8xX11d/movie-banner.jpg"
+
+# Database Storage File (Simple SQLite Database)
+DB_NAME = "movie_bot.db"
+
